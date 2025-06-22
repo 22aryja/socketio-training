0b96477639f8b1d7fd8d25e5fc63a3a6ce7d2065
22aryja <dagerson19@mail.ru>
2025-06-22 18:08:39 +0500

    chore: ngrok support

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import path from "path";
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
@@ -10,5 +10,8 @@ export default defineConfig({
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
+    },
+    server: {
+        allowedHosts: true,
     },
 });
